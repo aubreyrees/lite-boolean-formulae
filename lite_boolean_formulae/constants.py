fb4584c42acfab449d9b3909e7ef878f233b0d21
Aubrey Stark-Toller <aubrey@deepearth.uk>
2016-04-22 00:56:05 +0100

    Added py2 compatibability + added hash functions

--- lite_boolean_formulae/constants.py
+++ lite_boolean_formulae/constants.py
@@ -1,5 +1,5 @@
+import sys
 from .utils.immutable_class import ImmutableClass
-
 
 class TautologyClass(ImmutableClass):
     _frozen = True
@@ -10,14 +10,30 @@ class TautologyClass(ImmutableClass):
     def __or__(self, obj):
         return self
 
-    def __bool__(self):
-        return True
-
-    def __repr__(self):
+    def __repr__(self):             # pragma: no cover
         return 'Tautology'
 
     def __invert__(self):
         return Contradiction
+
+    def __hash__(self):
+        return 1
+
+    if sys.version_info < (3, 0):   # pragma: no cover
+        def __unicode__(self):
+            return u'Tautology'
+
+        def __str__(self):
+            return u'Tautology'.encode("utf-8")
+
+        def __nonzero__(self):
+            return True
+    else:                           # pragma: no cover
+        def __str__(self):
+            return u'Tautology'
+
+        def __bool__(self):
+            return True
 
 
 class ContradictionClass(ImmutableClass):
@@ -29,14 +45,30 @@ class ContradictionClass(ImmutableClass):
     def __or__(self, obj):
         return obj
 
-    def __bool__(self):
-        return False
-
-    def __repr__(self):
+    def __repr__(self):             # pragma: no cover
         return 'Contradiction'
 
     def __invert__(self):
         return Tautology
+
+    def __hash__(self):
+        return 0
+
+    if sys.version_info < (3, 0):   # pragma: no cover
+        def __unicode__(self):
+            return u'Contradiction'
+
+        def __str__(self):
+            return u'Contradiction'.encode("utf-8")
+
+        def __nonzero__(self):
+            return False
+    else:                           # pragma: no cover
+        def __str__(self):
+            return u'Contradiction'
+
+        def __bool__(self):
+            return False
 
 
 Tautology = TautologyClass()
