918e8985836021bfcd5348d36c4ce0a485836fbb
Aubrey Stark-Toller <aubrey@deepearth.uk>
2016-04-22 12:24:39 +0100

    Added Tautology and Contradiction hashes to tests

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -1,4 +1,4 @@
-from lite_boolean_formulae import L
+from lite_boolean_formulae import L, Tautology, Contradiction
 
 def test_literal_hash_same_for_the_same_literal():
     assert hash(L("x")) == hash(L("x"))
@@ -14,3 +14,11 @@ def test_formula_hash_same_for_the_same_formula():
 
 def test_formula_hash_different_for_the_different_formula():
     assert hash(L("a") & L("b")) != hash(L("y") & L("x"))
+
+
+def test_tautology_hash():
+    assert hash(Tautology) == hash(Tautology)
+
+
+def test_contradiction_hash():
+    assert hash(Contradiction) == hash(Contradiction)
